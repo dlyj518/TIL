9d0f7aee739109b7c8a223760601d171c990b3ab
dlyj518 <dlyj518@gmail.com>
2022-07-19 15:12:56 +0900

    0719

--- startcamp/test.py
+++ startcamp/test.py
@@ -1,7 +1,18 @@
-from hello import greeting, player
-from hello import bot
+from calendar import c
 
-print(greeting)
-print(player)
 
-bot()
+def st(r):
+    global c
+    c += 1
+    for i in tr[int(r)]:
+        if i:st(i)
+    return c
+
+for t in range(int(input())):
+    e,n = map(int,input().split())
+    nd = list(map(int,input().split()))
+    tr=[[] for _ in range(e+2)]
+    for i in range(0,e*2,2):
+        tr[nd[i]].append(nd[i+1])
+        c=0
+    print(f'#{t+1} {st(n)}')
