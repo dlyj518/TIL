0c3968dbb6817c55be9077622f807577c5c8857d
dlyj518 <dlyj518@gmail.com>
2022-07-22 15:50:43 +0900

    0722

--- startcamp/test.py
+++ startcamp/test.py
@@ -1,18 +1,16 @@
-from calendar import c
+def in_order(x):
+    c, rs = 0, [0,0]
+    if x<=n:
+        in_order(x*2)
+        c += 1
+        if x==1 : 
+            rs[0] = c
+        elif x==n//2 : 
+            rs[1] = c
+        in_order(x*2+1)
+    return(c, rs)
 
-
-def st(r):
-    global c
-    c += 1
-    for i in tr[int(r)]:
-        if i:st(i)
-    return c
-
-for t in range(int(input())):
-    e,n = map(int,input().split())
-    nd = list(map(int,input().split()))
-    tr=[[] for _ in range(e+2)]
-    for i in range(0,e*2,2):
-        tr[nd[i]].append(nd[i+1])
-        c=0
-    print(f'#{t+1} {st(n)}')
+t = int(input())
+for i in range(t):
+    n=int(input())
+    in_order(1)
