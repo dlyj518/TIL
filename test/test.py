78602b0a5976e1ab8175fc53e19be618df5019a5
dlyj518 <dlyj518@gmail.com>
2022-07-25 16:32:15 +0900

    0725

--- test/test.py
+++ test/test.py
@@ -1,12 +1,3 @@
-x = 0
-def f1():
-    x = 1
-    def f2():
-        nonlocal x
-        x = 2
-    print(x)
-    f2()
-    print(x)
-print(x)
-f1()
-print(x)
+x = int(input())
+for i in range(x):
+    print(' '*(x-i) + '*'*(i+1))
