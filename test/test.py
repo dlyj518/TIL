260e9565bfe5bc210766c8844c6bf0568891d99d
dlyj518 <dlyj518@gmail.com>
2022-07-28 17:23:39 +0900

    0728

--- test/test.py
+++ test/test.py
@@ -1,25 +1,7 @@
-class Doggy:
-    birth_of_dogs = 0
-    num_of_dogs = 0
-    def __init__(self, name, type):
-        self.name = name
-        self.type = type
-        Doggy.num_of_dogs += 1
+participants =  [3, 7, 100, 21, 13, 6, 5, 7, 5, 6, 3, 13, 21]
 
-    def bark(self):
-        print('bow!')
-    
-    @classmethod
-    def birth_dog(cls):
-        cls.num_of_dogs += 1
-        cls.birth_of_dogs += 1
-    
-    @classmethod
-    def death_dog(cls):
-        cls.num_of_dogs -= 1
-    
-    def get_status():
-        print(f'태어난 개 : {Doggy.birth_of_dogs}마리/n총 개 : {Doggy.num_of_dogs}마리')
-
-sangeun = Doggy('상근이', '삽살개')
-print(Doggy.get_status())
+kkdk = []
+for i in participants:
+    if i in kkdk: kkdk.remove(i)
+    else: kkdk.append(i)
+print(kkdk[0])
